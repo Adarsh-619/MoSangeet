4aec3f2df8947948158b5f6a14046aa18e159725
Shivam Gupta <shivam0gupta1@gmail.com>
2023-05-13 11:22:03 +0530

    SPLASSCREEN ADDED WITH HOMESCREEN AND ONLINE FEATURES

---
+++ app/src/main/java/com/mosangeet/Webviews/Demand.java
@@ -0,0 +1,36 @@
+package com.mosangeet.Webviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.mosangeet.R;
+
+public class Demand extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demand);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.jiosaavn.com/featured/trending-today/I3kvhipIy73uCJW60TJk1Q__");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
